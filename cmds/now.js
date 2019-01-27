482bafdff8d32479a10be11e99e7296c40cb08d9
Dominic DeChristopher <philliesfan5000@gmail.com>
2019-01-27 13:33:09 -0500

    NEW CHANGES

--- cmds/now.js
+++ cmds/now.js
@@ -1,7 +1,7 @@
 
 module.exports.run = async (bot,message,args) => {
 
-message.channel.send("<@&539146159521005590> // Scrim starting now!");
+message.channel.send("<@&539146159521005590> // A scrim is starting now!");
 }
 
 
